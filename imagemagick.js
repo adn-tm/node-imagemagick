d6143e4a99f6590c464baa6c1d576127571a06c3
Jun Woong <woong.jun@gmail.com>
2017-03-25 22:53:10 +0900

    Ignore "clipping path" while parsing identify result

--- imagemagick.js
+++ imagemagick.js
@@ -111,7 +111,7 @@ function parseIdentify(input) {
   for (i in lines) {
     currentLine = lines[i];
     indent = currentLine.search(/\S/);
-    if (indent >= 0) {
+    if (indent > 0) {
       comps = currentLine.split(': ');
       if (indent > prevIndent) indents.push(indent);
       while (indent < prevIndent && props.length) {
